50585e96463ed0bbb2c1e53838bb926c2ad8ff9f
Boker <39336096+Boker-D@users.noreply.github.com>
2018-06-06 00:37:12 +0300

    Update star.js

--- star.js
+++ star.js
@@ -1319,8 +1319,7 @@ if(message.content.split(' ')[0] == '~bc') {
         });
     }
 })
-           if (message.content.startsWith(prefix + "id")) {
-    
+     if (message.content === "~id") {    
                 let member = message.mentions.members.first();
               	 if(!message.channel.guild) return message.reply('**انتا تبغا توقف البوت ولا تستهبل رح السيرفر تشتغل قفطتك هاه يا ذكي**');
     const zmillisCreated = new Date().getTime() - message.author.createdAt.getTime();
