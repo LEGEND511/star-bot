836bbeb152b06be822b40cf0893b0d17ad38597f
Boker <39336096+Boker-D@users.noreply.github.com>
2018-06-09 21:08:52 +0300

    Update star.js

--- star.js
+++ star.js
@@ -1074,22 +1074,6 @@ if(ra3d.content.startsWith(prefix + 'cc')) {
               }
             }
        });
-client.on('message', ra3d => {
-var prefix = "=";
-                        let args = ra3d.content.split(" ").slice(1).join(" ")
-if(ra3d.content.startsWith(prefix + 'dc')) {
-    if(!args) return ra3d.channel.send('`يرجي اختيار كم لون تريد حذفه`');
-             if (!ra3d.member.hasPermission('MANAGE_ROLES')) return ra3d.channel.sendMessage('`**⚠ | `[MANAGE_ROLES]` لا يوجد لديك صلاحية**'); 
-              ra3d.channel.send(`**❎ |Deleted __${args}__ Colors**`);
-                  setInterval(function(){})
-                    let count = 0;
-                    let ecount = 0;
-          for(let x = 1; x < `${parseInt(args)+1}`; x++){
-            ra3d.guild.deleteRole({name:x,
-              color: 'RANDOM'})
-              }
-            }
-       });
 client.on('ready',  () => {
   console.log('By : Boker');
   console.log(`Logged in as * [ " ${client.user.username} " ] servers! [ " ${client.guilds.size} " ]`);
