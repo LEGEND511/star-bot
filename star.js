8c697786acb2aa061d29af3d1c45c92c40028492
Boker <39336096+Boker-D@users.noreply.github.com>
2018-06-21 01:24:33 +0300

    Update star.js

--- star.js
+++ star.js
@@ -1109,7 +1109,6 @@ client.on('message', message => {
     });
  client.on('message', message => {
               if(!message.channel.guild) return;
-    var prefix = "$";
     if(message.content.startsWith(prefix + 'bc3')) {
     if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
   if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**للأسف لا تمتلك صلاحية** `ADMINISTRATOR`' );
